b8a2995c2c0b94da037bbe4a76221cd92bf3783d
Mohammed Abdulrazeg <github@mzawi.me>
2024-12-04 02:17:06 +0000

    Forget the part 2 of ex1 and ex2

--- Ex1/src/main/java/tech/mozek/ex1/Main.java
+++ Ex1/src/main/java/tech/mozek/ex1/Main.java
@@ -8,6 +8,8 @@ import tech.mozek.ReadInput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -15,15 +17,17 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            logger.info("Solution to sample is {}", solution(ReadInput.readSampleInput()));
-            logger.info("Solution to input is {}", solution(ReadInput.readInput()));
+            logger.info("Part 1 solution to sample is {}", part1(ReadInput.readSampleInput()));
+            logger.info("Part 1 solution to input is {}", part1(ReadInput.readInput()));
+            logger.info("Part 2 solution to sample is {}", part2(ReadInput.readSampleInput()));
+            logger.info("Part 2 solution to input is {}", part2(ReadInput.readInput()));
         } catch (IOException e) {
             logger.error("Error reading input", e);
             throw new RuntimeException(e);
         }
     }
 
-    public static long solution(Stream<String> input) throws IOException {
+    public static long part1(Stream<String> input) throws IOException {
         var inputList = input.toList();
         logger.info("There are {} lines", inputList.size());
         List<Integer> left = new ArrayList<>();
@@ -40,5 +44,21 @@ public class Main {
         return Streams.
                 zip(left.stream(), right.stream(), (l, r) -> Math.abs(l - r))
                 .reduce(Integer::sum).get();
+    }
+
+    public static long part2(Stream<String> input) throws IOException {
+        var inputList = input.toList();
+        logger.info("There are {} lines", inputList.size());
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        inputList.forEach(line -> {
+            if (line.isBlank()) return;
+            String[] split = line.split("\\s+");
+            left.add(Integer.parseInt(split[0]));
+            right.add(Integer.parseInt(split[1]));
+        });
+        var rightMap = right.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return left.stream().map(l -> l * rightMap.getOrDefault(l, 0L)).reduce(Long::sum).get();
+
     }
 }
--- Ex2/src/main/java/tech/mozek/ex1/Main.java
+++ Ex2/src/main/java/tech/mozek/ex1/Main.java
@@ -6,7 +6,10 @@ import org.slf4j.LoggerFactory;
 import tech.mozek.ReadInput;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -14,15 +17,17 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            logger.info("Solution to sample is {}", solution(ReadInput.readSampleInput()));
-            logger.info("Solution to input is {}", solution(ReadInput.readInput()));
+            logger.info("Part 1 solution to sample is {}", part1(ReadInput.readSampleInput()));
+            logger.info("Part 1 solution to input is {}", part1(ReadInput.readInput()));
+            logger.info("Part 2 solution to sample is {}", part2(ReadInput.readSampleInput()));
+            logger.info("Part 2 solution to input is {}", part2(ReadInput.readInput()));
         } catch (IOException e) {
             logger.error("Error reading input", e);
             throw new RuntimeException(e);
         }
     }
 
-    public static int solution(Stream<String> input) throws IOException {
+    public static int part1(Stream<String> input) throws IOException {
         return input
                 .map(line -> line.split("\\s+"))
                 .map(line -> Arrays.stream(line).map(Integer::parseInt).toList())
@@ -33,5 +38,29 @@ public class Main {
                     if (diffs.stream().allMatch(d -> d <= 3 && d >= 1)) return 1;
                     return 0;
                 }).reduce(Integer::sum).get();
+    }
+
+    public static int part2(Stream<String> input) throws IOException {
+        return input
+                .map(line -> line.split("\\s+"))
+                .map(line -> Arrays.stream(line).map(Integer::parseInt).toList())
+                .map(line -> {
+                    var hasSafeOption = Stream.concat(Stream.of(line), generatePermutations(line)).anyMatch(ll -> {
+                        var diffs = Streams
+                                .zip(ll.stream(), ll.stream().skip(1), (l, r) -> l - r).toList();
+                        if (diffs.stream().allMatch(d -> d <= -1 && d >= -3)) return true;
+                        return diffs.stream().allMatch(d -> d <= 3 && d >= 1);
+                    });
+                    return hasSafeOption ? 1 : 0;
+                }).reduce(Integer::sum).get();
+    }
+
+    public static Stream<List<Integer>> generatePermutations(List<Integer> numbers) {
+        return IntStream.range(0, numbers.size())
+                .mapToObj(i -> {
+                    List<Integer> permutation = new ArrayList<>(numbers);
+                    permutation.remove(i);
+                    return permutation;
+                });
     }
 }
